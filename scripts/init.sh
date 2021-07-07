1642e3dfdb06a2f55ceb7626742174d2cf2e98ac
billy rennekamp <billy.rennekamp@gmail.com>
2021-07-07 15:36:37 +0200

    updated config to work but still kind of sucks

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
 rm -rf ~/.pooltoy
-pooltoy init mynode  --home ~/.pooltoy --chain-id pooltoy-5
+
+pooltoy config keyring-backend test
+pooltoy config chain-id pooltoy-5
+
+pooltoy config
+
+pooltoy init mynode --chain-id pooltoy-5
 
 jq -c '.balances[]' accounts.json | while read i; do
-
-    echo "y" | pooltoy keys add $(echo "$i" | jq -r ".name") --keyring-backend test --home ~/.pooltoy 
-    pooltoy add-genesis-account $(pooltoy keys show $(echo "$i" | jq -r ".name") --address --keyring-backend test --home ~/.pooltoy) $(echo $i | jq -r '.coins | map(.amount+.denom) | join(",")') --keyring-backend test --home ~/.pooltoy 
+    echo "y" | pooltoy keys add $(echo "$i" | jq -r ".name") 
+    pooltoy add-genesis-account $(pooltoy keys show $(echo "$i" | jq -r ".name") --address) $(echo $i | jq -r '.coins | map(.amount+.denom) | join(",")') 
 done
 
-pooltoy gentx alice 100000000stake --home ~/.pooltoy --chain-id pooltoy-5 --keyring-backend test 
-pooltoy collect-gentxs --home ~/.pooltoy
-pooltoy validate-genesis --home ~/.pooltoy
+pooltoy gentx alice 100000000stake --chain-id pooltoy-5
+pooltoy collect-gentxs
+pooltoy validate-genesis # test to make sure the genesis file is correctly formatted

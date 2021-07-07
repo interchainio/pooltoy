0f1c76ab349f6dbaa57e91d8c2eaa1c8021b7604
billy rennekamp <billy.rennekamp@gmail.com>
2021-07-07 14:54:00 +0200

    updated SDK and preparing for testing

--- go.mod
+++ go.mod
@@ -3,9 +3,9 @@ module github.com/interchainberlin/pooltoy
 go 1.15
 
 require (
-	github.com/cosmos/cosmos-sdk v0.42.3
+	github.com/cosmos/cosmos-sdk v0.42.6
 	github.com/gogo/protobuf v1.3.3
-	github.com/golang/protobuf v1.5.1
+	github.com/golang/protobuf v1.5.2
 	github.com/gorilla/mux v1.8.0
 	github.com/grpc-ecosystem/grpc-gateway v1.16.0
 	github.com/kr/text v0.2.0 // indirect
@@ -13,14 +13,14 @@ require (
 	github.com/spf13/cast v1.3.1
 	github.com/spf13/cobra v1.1.3
 	github.com/stretchr/testify v1.7.0
-	github.com/tendermint/tendermint v0.34.8
+	github.com/tendermint/tendermint v0.34.11
 	github.com/tendermint/tm-db v0.6.4
 	github.com/tmdvs/Go-Emoji-Utils v1.1.0
-	golang.org/x/sys v0.0.0-20201211090839-8ad439b19e0f // indirect
 	golang.org/x/term v0.0.0-20201210144234-2321bbc49cbf // indirect
 	google.golang.org/genproto v0.0.0-20210319143718-93e7006c17a6
-	google.golang.org/grpc v1.36.0
+	google.golang.org/grpc v1.37.0
 )
 
 replace github.com/gogo/protobuf => github.com/regen-network/protobuf v1.3.3-alpha.regen.1
+
 replace google.golang.org/grpc => google.golang.org/grpc v1.33.2
--- scripts/init.sh
+++ scripts/init.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
-[ -z "$CHAIN_DIR" ] && CHAIN_DIR="~/.pooltoy"
-[ -z "$CHAIN_ID" ] && CHAIN_ID="pooltoy-5"
-[ -z "$KEYRING" ] && CHAIN_ID="test"
 
-rm -rf $CHAIN_DIR
-pooltoy init mynode  --home $CHAIN_DIR --chain-id $CHAIN_ID
+rm -rf ~/.pooltoy
+pooltoy init mynode  --home ~/.pooltoy --chain-id pooltoy-5
 
 jq -c '.balances[]' accounts.json | while read i; do
 
-    echo "y" | pooltoy keys add $(echo "$i" | jq -r ".name") --keyring-backend $KEYRING --home $CHAIN_DIR 
-    pooltoy add-genesis-account $(pooltoy keys show $(echo "$i" | jq -r ".name") --address --keyring-backend $KEYRING --home $CHAIN_DIR) $(echo $i | jq -r '.coins | map(.amount+.denom) | join(",")') --keyring-backend $KEYRING --home $CHAIN_DIR 
+    echo "y" | pooltoy keys add $(echo "$i" | jq -r ".name") --keyring-backend test --home ~/.pooltoy 
+    pooltoy add-genesis-account $(pooltoy keys show $(echo "$i" | jq -r ".name") --address --keyring-backend test --home ~/.pooltoy) $(echo $i | jq -r '.coins | map(.amount+.denom) | join(",")') --keyring-backend test --home ~/.pooltoy 
 done
 
-pooltoy gentx alice 100000000stake --home $CHAIN_DIR --chain-id $CHAIN_ID --keyring-backend $KEYRING 
-pooltoy collect-gentxs --home $CHAIN_DIR
-pooltoy validate-genesis --home $CHAIN_DIR
+pooltoy gentx alice 100000000stake --home ~/.pooltoy --chain-id pooltoy-5 --keyring-backend test 
+pooltoy collect-gentxs --home ~/.pooltoy
+pooltoy validate-genesis --home ~/.pooltoy

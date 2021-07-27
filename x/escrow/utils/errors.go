e0a42c479a4fe1ed09a5071a25e15117e179f522
yaruwang <yaruwang.materials@gmail.com>
2021-07-27 18:31:14 +0200

    parse emojiStr into coins

--- x/escrow/utils/errors.go
+++ x/escrow/utils/errors.go
@@ -2,4 +2,9 @@ package utils
 
 import sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 
-var ErrParsEscrowEmoji  = sdkerrors.Register("escrow", 106, "parse emoji failed")
+var ErrParseEmojiandNum  = sdkerrors.Register("escrow", 106, "parse emoji string into numbers and emojis failed")
+var ErrParseEmojiToCoins  = sdkerrors.Register("escrow", 107, "parse emoji string into coins failed")
+
+var ErrEmojiCoinsCheck  = sdkerrors.Register("escrow", 108, "emoji coins check failed")
+
+var ErrEmojiStr  = sdkerrors.Register("escrow", 109, "parse emoji string into emoji failed")
--- x/escrow/utils/utils_test.go
+++ x/escrow/utils/utils_test.go
@@ -1,97 +1,88 @@
 package utils
 
 import (
-	"errors"
-	"fmt"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"testing"
-
 )
 
-func TestParseCoins_0(t *testing.T) {
-	coins := "1🆗200🍍"
-	res, err := ParseCoins(coins)
-	if err != nil {
-		t.Fatal(err)
+func TestParseCoins_Cases_Pass(t *testing.T) {
+	type ParseCase struct {
+		EmoStr string
+		Coins  sdk.Coins
 	}
 
-	if r :=res[0].Amount.BigInt().Cmp(sdk.NewInt(1).BigInt()); r!=0{
-		t.Fatal(res[1].Amount.BigInt())
-	}
-	if res[0].Denom != "🆗"{
-		t.Fatal(res[0].Denom)
+	samples := []ParseCase{
+		{
+			EmoStr: "1🆗",
+			Coins: []sdk.Coin{
+				{Denom: "🆗", Amount: sdk.NewInt(1)},
+			},
+		},
+		{
+			EmoStr: "🆗🍍",
+			Coins: []sdk.Coin{
+				{Denom: "🆗", Amount: sdk.NewInt(1)},
+				{Denom: "🍍", Amount: sdk.NewInt(1)},
+			},
+		},
+		{
+			EmoStr: "12🆗🍍",
+			Coins: []sdk.Coin{
+				{Denom: "🆗", Amount: sdk.NewInt(12)},
+				{Denom: "🍍", Amount: sdk.NewInt(1)},
+			},
+		},
+		{
+			EmoStr: "12" + "\U0001f1e6\U0001f1f2",
+			Coins: []sdk.Coin{
+				{Denom: "\U0001f1e6\U0001f1f2", Amount: sdk.NewInt(12)},
+			},
+		},
+
+		{
+			EmoStr: "10\U0001f9d1\u200d\U0001f3a8" +
+				"40\U0001f632" + "\U0001f1e6\U0001f1fa" + "\U0001f3a8" + "2\U0001f9d6\U0001f3fb\u200d\u2640\ufe0f",
+			Coins: []sdk.Coin{
+				{Denom: "\U0001f9d1\u200d\U0001f3a8", Amount: sdk.NewInt(10)},
+				{Denom: "\U0001f632", Amount: sdk.NewInt(40)},
+				{Denom: "\U0001f1e6\U0001f1fa", Amount: sdk.NewInt(1)},
+				{Denom: "\U0001f3a8", Amount: sdk.NewInt(1)},
+				{Denom: "\U0001f9d6\U0001f3fb\u200d\u2640\ufe0f", Amount: sdk.NewInt(2)},
+			},
+		},
 	}
 
-	if r :=res[1].Amount.BigInt().Cmp(sdk.NewInt(200).BigInt()); r!=0{
-		t.Fatal(res[1].Amount.BigInt())
-	}
-	if res[1].Denom != "🍍"{
-		t.Fatal(res[1].Denom)
+	for i, sample := range samples {
+		coins, err := GetCoinsWithCheck(sample.EmoStr)
+		if err != nil {
+			t.Fatalf("failed case %d", i)
+		}
+
+		if !coins.IsEqual(sample.Coins) {
+			t.Fatalf("failed case %d: %s", i, coins.String())
+		}
 	}
 }
 
+func TestParseCoins_Cases_Fail(t *testing.T) {
+	str := []string{
+		"",
+		"-123🆗",    // negative
+		"0🆗",       // zero
+		"1e10🆗",    // scientific notation
+		"1.2🆗",     // decimal
+		"1,223🆗",   // formatted
+		"1,223",     // no emoji
+		"1🆗🆗",     // duplicate emoji
+		"1🆗2🍍3🆗", // duplicate emoji
+		//	"00002🆗⭐", // leading zero  //todo if allow ???
+		"01 🆗", //space
+	}
 
-func TestParseCoins_1(t *testing.T) {
-	coins := "🆗200🍍"
-	res, err := ParseCoins(coins)
-	if err != nil {
-		t.Fatal(err)
-	}
-	fmt.Println(res.String())
-
-	if r :=res[0].Amount.BigInt().Cmp(sdk.NewInt(1).BigInt()); r!=0{
-		t.Fatal(res[1].Amount.BigInt())
-	}
-	if res[0].Denom != "🆗"{
-		t.Fatal(res[0].Denom)
-	}
-
-	if r :=res[1].Amount.BigInt().Cmp(sdk.NewInt(200).BigInt()); r!=0{
-		t.Fatal(res[1].Amount.BigInt())
-	}
-	if res[1].Denom != "🍍"{
-		t.Fatal(res[1].Denom)
+	for i, s := range str {
+		_, err := GetCoinsWithCheck(s)
+		if err == nil {
+			t.Fatalf("failed case %d", i)
+		}
 	}
 }
-
-func TestParseCoins_2(t *testing.T) {
-	coins := "🆗🍍"
-	res, err := ParseCoins(coins)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	if r :=res[0].Amount.BigInt().Cmp(sdk.NewInt(1).BigInt()); r!=0{
-		t.Fatal(res[1].Amount.BigInt())
-	}
-	if res[0].Denom != "🆗"{
-		t.Fatal(res[0].Denom)
-	}
-
-	if r :=res[1].Amount.BigInt().Cmp(sdk.NewInt(1).BigInt()); r!=0{
-		t.Fatal(res[1].Amount.BigInt())
-	}
-	if res[1].Denom != "🍍"{
-		t.Fatal(res[1].Denom)
-	}
-}
-
-// -----------------------------------------------------------------------------
-//fail cases
-// -----------------------------------------------------------------------------
-func TestParseCoins_3(t *testing.T) {
-	coins := "1🆗0🍍"
-	_, err := ParseCoins(coins)
-	if !errors.Is(err, ErrParsEscrowEmoji) {
-		t.Fatal(err)
-	}
-}
-
-func TestParseCoins_4(t *testing.T) {
-	coins := "0🆗0🍍"
-	_, err := ParseCoins(coins)
-	if !errors.Is(err, ErrParsEscrowEmoji) {
-		t.Fatal(err)
-	}
-}
-

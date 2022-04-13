d72196d0bbf0ff2bd408450ade58b2dd82e49dff
Julien Robert <julien@rbrt.fr>
2022-04-13 14:56:13 +0200

    replace `tx send` to `tx bank send`

--- happytrail.sh
+++ happytrail.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # pooltoy tx faucet mintfor $(pooltoy keys show bob -a) 🚀 --from alice -y | jq ".txhash" |  xargs $(sleep 6) pooltoy q tx | jq ".raw_log"
-# pooltoy tx send bob $(pooltoy keys show alice -a) 1🚀 --from bob -y | jq ".txhash" |  xargs $(sleep 6) pooltoy q tx
-# pooltoy tx send you $(pooltoy keys show me -a) 1token --from you -y | jq ".txhash" |  xargs $(sleep 6) pooltoy q tx
-# pooltoy tx send me $(pooltoy keys show who -a) 1token --from me | jq ".txhash" |  xargs $(sleep 6) pooltoy q tx
+# pooltoy tx bank sendbob $(pooltoy keys show alice -a) 1🚀 --from bob -y | jq ".txhash" |  xargs $(sleep 6) pooltoy q tx
+# pooltoy tx bank sendyou $(pooltoy keys show me -a) 1token --from you -y | jq ".txhash" |  xargs $(sleep 6) pooltoy q tx
+# pooltoy tx bank sendme $(pooltoy keys show who -a) 1token --from me | jq ".txhash" |  xargs $(sleep 6) pooltoy q tx
 
 echo "make alice admin by alice"
 pooltoy tx pooltoy create-user $(pooltoy keys show alice -a) true billy billy@interchain.berlin --from alice -y | jq ".txhash" |  xargs $(sleep 6) pooltoy q tx | jq ".raw_log"
@@ -26,7 +26,7 @@ echo "how long til alice can mint?"
 pooltoy q faucet when-brrr $(pooltoy keys show alice -a)
 
 echo "can bob send to alice?"
-pooltoy tx send bob $(pooltoy keys show alice -a) 1🚀 --from bob -y | jq ".txhash" |  xargs $(sleep 6) pooltoy q tx | jq ".raw_log"
+pooltoy tx bank send bob $(pooltoy keys show alice -a) 1🚀 --from bob -y | jq ".txhash" |  xargs $(sleep 6) pooltoy q tx | jq ".raw_log"
 
 # echo "should fail"
 # pooltoy tx faucet mintfor $(pooltoy keys show carol -a) 🚀 --from alice -y | jq ".txhash" |  xargs $(sleep 6) pooltoy q tx | jq ".raw_log"
@@ -36,21 +36,21 @@ pooltoy tx send bob $(pooltoy keys show alice -a) 1🚀 --from bob -y | jq ".txh
 # pooltoy tx faucet mintfor $(pooltoy keys show alice -a) 💸 --from carol -y | jq ".txhash" |  xargs $(sleep 6) pooltoy q tx | jq ".raw_log"
 
 # # this one should fail
-# echo "should fail"
+# echo "should fail"``
 # pooltoy tx faucet mintfor $(pooltoy keys show alice -a) 💸 --from carol -y | jq ".txhash" |  xargs $(sleep 6) pooltoy q tx | jq ".raw_log"
 
 # pooltoy q account $(pooltoy keys show alice -a)
 # pooltoy q account $(pooltoy keys show bob -a)
 # pooltoy q account $(pooltoy keys show carol -a)
 
-# pooltoy tx send alice $(pooltoy keys show bob -a) 1💸 --from alice -y | jq ".txhash" |  xargs $(sleep 6) pooltoy q tx | jq ".raw_log"
-# pooltoy tx send bob $(pooltoy keys show carol -a) 1🚀 --from bob -y | jq ".txhash" |  xargs $(sleep 6) pooltoy q tx | jq ".raw_log"
-# pooltoy tx send carol $(pooltoy keys show alice -a) 1🌝 --from carol -y | jq ".txhash" |  xargs $(sleep 6) pooltoy q tx | jq ".raw_log"
+# pooltoy tx bank sendalice $(pooltoy keys show bob -a) 1💸 --from alice -y | jq ".txhash" |  xargs $(sleep 6) pooltoy q tx | jq ".raw_log"
+# pooltoy tx bank sendbob $(pooltoy keys show carol -a) 1🚀 --from bob -y | jq ".txhash" |  xargs $(sleep 6) pooltoy q tx | jq ".raw_log"
+# pooltoy tx bank sendcarol $(pooltoy keys show alice -a) 1🌝 --from carol -y | jq ".txhash" |  xargs $(sleep 6) pooltoy q tx | jq ".raw_log"
 
 # pooltoy q account $(pooltoy keys show alice -a)
 # pooltoy q account $(pooltoy keys show bob -a)
 # pooltoy q account $(pooltoy keys show carol -a)
 
 # echo "should fail"
-# pooltoy tx send alice $(pooltoy keys show doug -a) 1🌝 --from alice -y | jq ".txhash" |  xargs $(sleep 6) pooltoy q tx | jq ".raw_log"
+# pooltoy tx bank sendalice $(pooltoy keys show doug -a) 1🌝 --from alice -y | jq ".txhash" |  xargs $(sleep 6) pooltoy q tx | jq ".raw_log"
 
